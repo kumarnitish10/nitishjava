021ab8cb124157ca5af6cc3a3d16276a86393412
kumarnitish10 <112919444+kumarnitish10@users.noreply.github.com>
2023-08-27 00:13:16 +0530

    Add files via upload

---
+++ Assignment1EvenOdd.java
@@ -0,0 +1,18 @@
+
+
+    class Assignment1EvenOdd {
+     public static void main(String[] args)
+    {
+        int num = 7;
+
+        if (num % 2 == 0) {
+ 
+            System.out.println("Entered Number is Even");
+        }
+ 
+        else {
+ 
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
